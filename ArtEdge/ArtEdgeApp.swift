ea9de1335e558241fbd1cb752f94fa5fbaefec1d
Nitin Gupta <niting1209@gmail.com>
2025-04-19 15:37:38 -0400

    Initial Commit

---
+++ ArtEdge/ArtEdgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtEdgeApp.swift
+//  ArtEdge
+//
+//  Created by Nitin Gupta on 4/19/25.
+//
+
+import SwiftUI
+
+@main
+struct ArtEdgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArtEdgeTests/ArtEdgeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArtEdgeTests.swift
+//  ArtEdgeTests
+//
+//  Created by Nitin Gupta on 4/19/25.
+//
+
+import Testing
+@testable import ArtEdge
+
+struct ArtEdgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
